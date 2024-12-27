1b2058db75b66e6fe371df01f7f961d31235e0a6
Ramesh Bojanapu <84425972+lovelyram0143@users.noreply.github.com>
2024-12-27 11:35:20 +0530

    Remove Whitenoise middleware and clear ALLOWED_HOSTS in settings.py; update vercel.json for version consistency

--- main_part/settings.py
+++ main_part/settings.py
@@ -27,14 +27,13 @@ SECRET_KEY = 'django-insecure-cqe@_28d21ton3+*5t7#n(^%+g&yw2k79sw)47=7sg^0z0z&-h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
 
 INSTALLED_APPS = [
     'sub_part', 
-    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -46,7 +45,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
